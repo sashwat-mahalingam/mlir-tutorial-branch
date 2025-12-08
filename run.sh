b214a6540f10a62d41f494a5ba9193d05932e6b0
Sashwat <msashwat@umich.edu>
2025-12-08 09:43:58 -0500

    last change

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ cgeist benchmarks/matmul.c -I $HOME/llvm-mlir-pgeist/lib/clang/18/include \
 ./build-ninja/tools/mlir-poly-tiling-opt benchmarks/matmul_affine_unraised.mlir --raise-to-affine -o benchmarks/matmul_affine.mlir
 ./build-ninja/tools/mlir-poly-tiling-opt benchmarks/matmul_affine.mlir --pre-tile-analysis -o benchmarks/matmul_affine.mlir
 
-# mlir-opt matmul_affine.mlir \
+# mlir-opt benchmarks/matmul_affine.mlir \
 #     --affine-loop-tile="tile-sizes=32,64,38" \
 #     --canonicalize \
 #     --cse \
-#     -o matmul_tiled.mlir
+#     -o benchmarks/matmul_tiled.mlir
     
-# polygeist-opt matmul_tiled.mlir --lower-affine | mlir-opt --convert-scf-to-cf | mlir-opt --convert-to-llvm | mlir-opt \
-#     --reconcile-unrealized-casts | mlir-translate --mlir-to-llvmir -o matmul.ll
+# polygeist-opt benchmarks/matmul_tiled.mlir --lower-affine | mlir-opt --convert-scf-to-cf | mlir-opt --convert-to-llvm | mlir-opt \
+#     --reconcile-unrealized-casts | mlir-translate --mlir-to-llvmir -o benchmarks/matmul.ll
