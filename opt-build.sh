c59e03383f28c70d8abcfdcc92bd8f26207ffa1c
Sashwat <msashwat@umich.edu>
2025-12-08 13:37:41 -0500

    minor

--- opt-build.sh
+++ opt-build.sh
@@ -1,31 +1,16 @@
 #!/bin/sh
-FIRST_TIME_BUILD=$1
-
-if [ "$FIRST_TIME_BUILD" = "--first-time" ]; then
-    first_time_build=true
-    build_deps_flag="ON"
-else
-    first_time_build=false
-    build_deps_flag="OFF"
-fi
-
 BUILD_SYSTEM="Ninja"
 BUILD_DIR=./build-`echo ${BUILD_SYSTEM}| tr '[:upper:]' '[:lower:]'`
 
-BUILD_SYSTEM="Ninja"
-
-if [ "$first_time_build" = true ]; then
-    rm -rf $BUILD_DIR
-    mkdir $BUILD_DIR
-else
-    cd $BUILD_DIR
-fi
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
 
 LLVM_BUILD_DIR=${HOME}/Polygeist/build/
 cmake -G $BUILD_SYSTEM .. \
     -DLLVM_DIR="$LLVM_BUILD_DIR/lib/cmake/llvm" \
     -DMLIR_DIR="$LLVM_BUILD_DIR/lib/cmake/mlir" \
-    -DBUILD_DEPS=${build_deps_flag} \
+    -DBUILD_DEPS="ON" \
     -DBUILD_SHARED_LIBS="OFF" \
     -DCMAKE_BUILD_TYPE=Debug \
     -DLLVM_INCLUDE_DIRS=${HOME}/llvm-mlir-pgeist/include/llvm \
